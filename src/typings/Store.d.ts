f4d3198049f566f5d5fd0e6fae3f0cc86fc52821
Andrei Taranenko <andrei_taranenko@epam.com>
2021-11-12 23:49:10 +0300

    Store typing update

--- src/typings/Store.d.ts
+++ src/typings/Store.d.ts
@@ -1,7 +1,25 @@
+import {
+  AsyncThunk,
+  AsyncThunkOptions,
+  AsyncThunkPayloadCreator,
+} from '@reduxjs/toolkit';
+
 import { AppStore } from 'store';
 
 declare module 'react-redux' {
   function useDispatch(): AppStore['dispatch']
 
   interface DefaultRootState extends ReturnType<AppStore['getState']> {}
+}
+
+declare module '@reduxjs/toolkit' {
+  export declare function createAsyncThunk<
+    Returned,
+    ThunkArg = void,
+    ThunkApiConfig extends AsyncThunkConfig = { state: AppState }
+  >(
+    typePrefix: string,
+    payloadCreator: AsyncThunkPayloadCreator<Returned, ThunkArg, ThunkApiConfig>,
+    options?: AsyncThunkOptions<ThunkArg, ThunkApiConfig>
+  ): AsyncThunk<Returned, ThunkArg, ThunkApiConfig>;
 }
